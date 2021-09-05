ec5d2c9d871e4962e47cd577ab5f0118cbde6de5
Sam Radford <smr1292@yahoo.com>
2021-09-05 13:55:18 -0400

    update person endpoint and get all person endpoint with sorting and pagination

--- src/main/java/com/radford/aba/modules/person/controller/PersonController.java
+++ src/main/java/com/radford/aba/modules/person/controller/PersonController.java
@@ -8,12 +8,18 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.radford.aba.modules.person.entity.Person;
@@ -41,6 +47,20 @@ public class PersonController {
 		return new ResponseEntity<>(createdPerson, HttpStatus.CREATED);
 	}
 	
+	@GetMapping("/person") 
+	public Page<Person> getAllPerson(
+			@RequestParam(defaultValue="1") Integer pageNumber,
+			@RequestParam(defaultValue="5") Integer pageSize,
+			@RequestParam(defaultValue="id") String sortBy,
+			@RequestParam(defaultValue="ASC") String sortDirection
+	) {
+		pageNumber--;
+		Sort.Direction direction = sortDirection.toLowerCase().equals("asc") ? Sort.Direction.ASC : Sort.Direction.DESC;
+		Pageable page = PageRequest.of(pageNumber, pageSize, Sort.by(direction, sortBy));
+		return personRepository.findAll(page);
+		
+	}
+	
 	@GetMapping("/person/{personId}") 
 	public ResponseEntity<?> getPersonById(@PathVariable Integer personId){
 		Optional<Person> personIfExists = personRepository.findById(personId);
@@ -50,5 +70,18 @@ public class PersonController {
 		}
 		ABAErrorMessage errorMessage = new ABAErrorMessage("No 'Person' found with ID: " + personId);
 		return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+	}
+	
+	@PutMapping("person/{personId}")
+	public ResponseEntity<?> updatePerson(@PathVariable Integer personId, @Valid @RequestBody Person person) {
+		if(personRepository.existsById(personId)) {
+			person.setId(personId);
+			Person savedPerson = personRepository.save(person);
+			return new ResponseEntity<>(savedPerson, HttpStatus.OK);
+		}
+		
+		ABAErrorMessage errorMessage = new ABAErrorMessage("No 'Person' found with ID: " + personId);
+		return new ResponseEntity<>(errorMessage, HttpStatus.NOT_FOUND);
+		
 	}
 }
--- src/main/java/com/radford/aba/modules/person/repository/PersonRepository.java
+++ src/main/java/com/radford/aba/modules/person/repository/PersonRepository.java
@@ -1,9 +1,13 @@
 package com.radford.aba.modules.person.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import com.radford.aba.modules.person.entity.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
+
+	Page<Person> findAll(Pageable page);
 
 }
